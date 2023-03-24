006518591f492c11058256ed9723b6b771968637
RAHMA993 <romaa200399@gmail.com>
2023-03-24 15:20:30 +0200

    Power file

---
+++ power.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float x, power;
+    printf("Please Enter The Number\n");
+    scanf("%f",&x);
+     power= x*x;
+     printf("The power of number %f is %f", x, power);
+    return 0;
+}
